46357e9dbd605936a9d695d28c5135379779b0b9
Felix Warner <fwarner2319@gmail.com>
2023-10-31 01:21:14 -0400

    Ensured back camera is used on mobile devices, and added in-document error display

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sight to Speech
+## Idea
+Phone read word from camera (expand upon this pls)
+## Sources
+- https://simpl.info/getusermedia/sources/js/main.js
--- src/index.js
+++ src/index.js
@@ -3,91 +3,91 @@ const backButton = document.getElementById('back-to-cam');
 const video = document.getElementById('vid');
 const imgCanvas = document.getElementById('imgCanvas');
 const image = document.getElementById('myImage');
+const errorMessage = document.querySelector("#you-got-trouble");
+const setErrorMessage = (message) => errorMessage.innerHTML = message?.toString();
 let height = 320;
 let width = 0;
 let streaming = false;
 
-let ImageRecorder;
-
-let capturedImage;
-
-
-videoButton.onclick = (ev)=>{
-        takePicture();
-        ev.preventDefault();
+videoButton.onclick = (ev) => {
+    takePicture();
+    ev.preventDefault();
 }
 
-backButton.onclick = (ev)=>{
+backButton.onclick = (ev) => {
     clearPhoto();
+    ev.preventDefault();
 }
 
-async function init(){
-    try{
-        const stream = await navigator.mediaDevices.getUserMedia({
-            audio:false,
-            video:true,
-            height:320,
-            width:240
-        }).then((stream) =>{
-            video.srcObject = stream;
-            window.stream = stream;
-        });
-    } catch (err){
-        console.log('error retrieving medai device');
-        console.log(err);
-    }
-    video.addEventListener('canplay', (event) =>{
-        if(!streaming){
-            width = video.videoWidth/ (video.videoHeight/height);
+// Very quick and dirty method of identifying which camera is the back camera
+function deviceIsBackCamera(deviceInfo) {
+    return deviceInfo.label.search("back") !== -1 && deviceInfo.kind === "videoinput";
+}
 
-            if(isNaN(width)) width=height/(4/3);
+async function init() {
+    try {
+        // https://simpl.info/getusermedia/sources/js/main.js
+        const deviceInfos = await navigator.mediaDevices.enumerateDevices();
+        const backCamera = deviceInfos.find(deviceIsBackCamera);
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                deviceId: backCamera ? {
+                    exact: backCamera.deviceId,
+                } : undefined,
+            },
+            height: 320,
+            width: 240
+        });
+        window.stream = stream;
+        video.srcObject = stream;
+    } catch (err) {
+        setErrorMessage(err);
+    }
+    video.addEventListener('canplay', (event) => {
+        if (!streaming) {
+            width = video.videoWidth / (video.videoHeight / height);
+
+            if (isNaN(width)) width = height / (4 / 3);
         }
     })
-
-
-
-
 }
 
-function clearPhoto(){
+function clearPhoto() {
     let context = imgCanvas.getContext('2d');
     imgCanvas.style.display = 'none';
     video.style.display = 'block';
     videoButton.disabled = false;
     videoButton.style.backgroundColor = 'aquamarine';
-    videoButton.style.display='block';
+    videoButton.style.display = 'block';
     backButton.style.display = 'none';
     let data = imgCanvas.toDataURL('image/png');
-    image.setAttribute('src',data);
+    image.setAttribute('src', data);
 }
 
-
-function takePicture(){
+function takePicture() {
     let context = imgCanvas.getContext('2d');
-    if(width&&height){
+    if (width && height) {
         imgCanvas.width = width;
         imgCanvas.height = height;
-        video.style.display='none';
-        imgCanvas.style.display='block';
-        backButton.style.display='block';
+        video.style.display = 'none';
+        imgCanvas.style.display = 'block';
+        backButton.style.display = 'block';
         videoButton.disabled = true;
-        videoButton.style.display='none';
+        videoButton.style.display = 'none';
         backButton.disabled = false;
-        videoButton.style.backgroundColor='grey';
-        context.drawImage(video,0,0,width,height);
+        videoButton.style.backgroundColor = 'grey';
+        context.drawImage(video, 0, 0, width, height);
         let data = imgCanvas.toDataURL('image/png');
-        image.setAttribute('src',data);
-    }else{
+        image.setAttribute('src', data);
+    } else {
         clearPhoto();
     }
 }
 
-
-function recordVideo(event){
-    console.log(event);
-    if(event){
-        console.log('tester for event.data');
-        console.log(typeof(event));
+function recordVideo(event) {
+    if (event) {
+        setErrorMessage(event);
         video.srcObject = null;
         let pictureDay = document.querySelector("picture-day");
         let newImage = document.createElement("img");
@@ -97,7 +97,10 @@ function recordVideo(event){
     }
 }
 
-
-window.onload = () =>{
-    init();
+window.onload = () => {
+    try {
+        init();
+    } catch (e) {
+        setErrorMessage(e.toString());
+    }
 }
