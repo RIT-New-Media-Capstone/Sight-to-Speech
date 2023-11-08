097ee7b3868478966c4b199712dcc67b798f0a5d
Felix Warner <fwarner2319@gmail.com>
2023-11-08 01:19:37 -0500

    Made UI scalable to any (at least portrait) screen size, cleaned up unnecessary code, implemented word highlighting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 const WORD_CONFIDENCE_THRESHOLD = 20;
+const HIGHLIGHT_OUTLINE_THICKNESS = 4;
 
-const videoButton = document.getElementById('take-pic');
-const backButton = document.getElementById('back-to-cam');
-const voiceButton = document.getElementById('speech-btn');
+const displayContainerOuter = document.getElementById('mainDisplayContainerOuter');
+const displayContainerInner = document.getElementById('mainDisplayContainerInner');
+const videoButton = document.getElementById('takePic');
+const backButton = document.getElementById('backToCam');
+const voiceButton = document.getElementById('speechBtn');
 const video = document.getElementById('vid');
+let onImgScreen = false;
+const hiddenImage = document.createElement('img');
 const imgCanvas = document.getElementById('imgCanvas');
-const image = document.getElementById('myImage');
-const errorMessage = document.querySelector("#you-got-trouble");
-let iconSize = 50;
+const imgCanvasCtx = imgCanvas.getContext('2d');
+const errorMessage = document.querySelector('#youGotTrouble');
 const setErrorMessage = (message) => errorMessage.innerHTML = message?.toString();
-let height = 640;
-let width = 0;
-let streaming = false;
+let videoWidth, videoHeight;
 
 const { createWorker, createScheduler } = Tesseract;
 const scheduler = createScheduler();
+
+function resizeDisplay() {
+    if (!(videoWidth && videoHeight)) return;
+    const { offsetWidth, offsetHeight, offsetLeft, offsetTop } = displayContainerOuter;
+    const newScale = Math.min(offsetWidth / videoWidth, offsetHeight / videoHeight);
+    const newWidth = videoWidth * newScale;
+    const newHeight = videoHeight * newScale;
+    displayContainerInner.style.width = `${newWidth}px`;
+    displayContainerInner.style.height = `${newHeight}px`;
+    displayContainerInner.style.left = `${(offsetWidth - newWidth) / 2 + offsetLeft}px`;
+    displayContainerInner.style.top = `${(offsetHeight - newHeight) / 2 + offsetTop}px`;
+    imgCanvasCtx.strokeStyle = 'red';
+    imgCanvasCtx.lineWidth = HIGHLIGHT_OUTLINE_THICKNESS / newScale;
+}
 
 videoButton.onclick = (ev) => {
     takePicture();
@@ -29,11 +45,11 @@ backButton.onclick = (ev) => {
 
 // Very quick and dirty method of identifying which camera is the back camera
 function deviceIsBackCamera(deviceInfo) {
-    return deviceInfo.label.search("back") !== -1 && deviceInfo.kind === "videoinput";
+    return deviceInfo.label.search('back') !== -1 && deviceInfo.kind === 'videoinput';
 }
 
 async function init() {
-    setErrorMessage("Initializing Tesseract.js");
+    setErrorMessage('Initializing Tesseract.js');
     const workerCount = 1;
     for (let i = 0; i < workerCount; i++) {
         const worker = createWorker();
@@ -45,7 +61,7 @@ async function init() {
         await worker.initialize('eng');
         scheduler.addWorker(worker);
     }
-    setErrorMessage("Initialized Tesseract.js; awaiting camera");
+    setErrorMessage('Initialized Tesseract.js; awaiting camera');
 
     try {
         // https://simpl.info/getusermedia/sources/js/main.js
@@ -54,9 +70,9 @@ async function init() {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: false,
             video: {
-                deviceId: backCamera ? {
-                    exact: backCamera.deviceId,
-                } : undefined,
+                deviceId: {
+                    exact: backCamera ? backCamera.deviceId : deviceInfos[Math.min(deviceInfos.length - 1, 2)].deviceId,
+                },
             },
             height: 1080,
             width: 1080
@@ -67,36 +83,33 @@ async function init() {
         setErrorMessage(err);
     }
 
-    setErrorMessage("Ready");
+    setErrorMessage('Ready');
     video.addEventListener('canplay', (event) => {
-        if (!streaming) {
-            width = video.videoWidth / (video.videoHeight / height);
-
-            if (isNaN(width)) width = height / (4 / 3);
-        }
+        videoWidth = video.videoWidth;
+        videoHeight = video.videoHeight;
+        imgCanvas.width = videoWidth;
+        imgCanvas.height = videoHeight;
+        resizeDisplay();
     });
 }
 
 function clearPhoto() {
-    let context = imgCanvas.getContext('2d');
     switchState();
-    let data = imgCanvas.toDataURL('image/png');
-    image.setAttribute('src', data);
-    setErrorMessage("Ready");
+    setErrorMessage('Ready');
 }
 
 async function takePicture() {
-    let context = imgCanvas.getContext('2d');
-    if (width && height) {
+    if (videoWidth && videoHeight) {
+        voiceButton.onclick = () => {};
+        imgCanvasCtx.drawImage(video, 0, 0);
+        hiddenImage.src = imgCanvas.toDataURL('image/png');
+        setErrorMessage('Parsing text');
         switchState();
-        context.drawImage(video, 0, 0, width, height);
-        let data = imgCanvas.toDataURL('image/png');
-        image.setAttribute('src', data);
-        setErrorMessage("Parsing text");
 
         const paragraphsFiltered = await parseCanvas(imgCanvas);
         console.log(paragraphsFiltered);
-        setErrorMessage(paragraphsFiltered.map(pgraph => pgraph.wordList.map(word => word.text).join(" ")).join(" / "));
+        sayIt(paragraphsFiltered);
+        voiceButton.onclick = () => sayIt(paragraphsFiltered);
     } else {
         clearPhoto();
     }
@@ -116,7 +129,7 @@ async function parseCanvas(canvas) {
                 const word = words[k];
                 const { confidence, text } = word;
                 // https://stackoverflow.com/questions/7349312/how-to-count-the-number-of-letters-in-a-random-string
-                const alphaNumericCount = text.replace(/[^0-9A-Za-z]/gi, "").length;
+                const alphaNumericCount = text.replace(/[^0-9A-Za-z]/gi, '').length;
                 if (confidence > WORD_CONFIDENCE_THRESHOLD && alphaNumericCount >= text.length - alphaNumericCount) {
                     wordList.push({
                         ...word,
@@ -133,51 +146,75 @@ async function parseCanvas(canvas) {
     return paragraphsFiltered;
 }
 
-function recordVideo(event) {
-    if (event) {
-        setErrorMessage(event);
-        video.srcObject = null;
-        let pictureDay = document.querySelector("picture-day");
-        let newImage = document.createElement("img");
-        newImage.src = URL.createObjectURL(event);
-        pictureDay.appendChild(newImage);
-        video.src = videoUrl;
+function sayIt(paragraphsFiltered) {
+    const utterances = paragraphsFiltered.map((pgraph, pgraphIndex) => {
+        let wordIndex = 0;
+        const { wordList } = pgraph;
+        const utterance = new SpeechSynthesisUtterance(wordList.map((word) => word.text.replace(/[^0-9A-Za-z\-]/gi, '')).join(' '));
+        utterance.onboundary = function (event) {
+            if (event.name === 'word') {
+                highlightWord(wordList[wordIndex++]);
+            }
+        };
+        utterance.onend = () => {
+            if (pgraphIndex >= paragraphsFiltered.length - 1) {
+                if (onImgScreen) {
+                    highlightWord();
+                }
+            } else {
+                speechSynthesis.speak(utterances[pgraphIndex + 1]);
+            }
+        }
+        return utterance;
+    });
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utterances.length > 0 ? utterances[0] : new SpeechSynthesisUtterance('Please try again'));
+}
+
+function highlightWord(word) {
+    imgCanvasCtx.drawImage(hiddenImage, 0, 0);
+    if (word) {
+        const { x0, y0, x1, y1 } = word.bbox;
+        imgCanvasCtx.beginPath();
+        imgCanvasCtx.rect(x0, y0, x1 - x0, y1 - y0);
+        imgCanvasCtx.stroke();
+        imgCanvasCtx.closePath();
+        setErrorMessage(word.text);
+        console.log(word.text);
+    } else {
+        setErrorMessage('(done)');
+        console.log('(done)');
     }
 }
 
-function switchState(){
-    //change to image state
-    if(imgCanvas.style.display== 'none'){
-        imgCanvas.width = width;
-        imgCanvas.height = height;
-        video.style.display = 'none';
+function switchState() {
+    speechSynthesis.cancel();
+    onImgScreen = !onImgScreen;
+    if (onImgScreen) {
         imgCanvas.style.display = 'block';
-        backButton.style.display = 'block';
-        voiceButton.style.display='block';
-        voiceButton.disabled = false;
+        video.style.display = 'none';
         videoButton.disabled = true;
         videoButton.style.display = 'none';
+        voiceButton.disabled = false;
+        voiceButton.style.display = 'block';
         backButton.disabled = false;
-        videoButton.style.backgroundColor = 'grey';
-    }else{
+        backButton.style.display = 'block';
+    } else {
         imgCanvas.style.display = 'none';
-        image.style.display = 'none';
         video.style.display = 'block';
         videoButton.disabled = false;
-        videoButton.style.backgroundColor = 'aquamarine';
         videoButton.style.display = 'block';
+        backButton.disabled = true;
         backButton.style.display = 'none';
-        voiceButton.disabled=true;
-        voiceButton.style.display='none';
+        voiceButton.disabled = true;
+        voiceButton.style.display = 'none';
     }
 }
 
+window.onresize = resizeDisplay;
 
-
-window.onload = () => {
-    try {
-        init();
-    } catch (e) {
-        setErrorMessage(e.toString());
-    }
+try {
+    init();
+} catch (e) {
+    setErrorMessage(e.toString());
 }
